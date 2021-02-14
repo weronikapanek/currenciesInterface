c12bab6c0e48c598436df5afb222e74d8442ebd8
weronikapanek <werkap8811@gmail.com>
2021-02-14 11:46:26 +0100

    creating favList, clear all button and night mode

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 const favlist = document.querySelector(".list");
+const favCurrency = document.querySelector(".ratesList")
 
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", () => {
+  favlist.textContent = "";
+});
+
+const themeSwitchButton = document.querySelector(".mode");
+themeSwitchButton.addEventListener("click", () => {
+  const body = document.querySelector("body");
+  body.classList.toggle("dark-mode");
+});
+
+function createAddFavoriteButton(name, bid, ask) {
+  const favButton = document.createElement("button");
+  favButton.className = "favbutton";
+  favButton.textContent = "⭐";
+  favButton.addEventListener("click", () => {
+    
+    const favCurrency = document.createElement("div");
+    favCurrency.className = "ratesList";
+    favCurrency.textContent = `${name} ${bid} ${ask}`;
+    if (favCurrency.textContent === favlist.textContent) {
+      return window.alert ('This currency has been already added')
+    } else {
+    favlist.appendChild(favCurrency);
+    }
+  });
+
+  return favButton;
+}
 
 function renderCurrency(name, bid, ask) {
   const favButton = createAddFavoriteButton(name, bid, ask);
